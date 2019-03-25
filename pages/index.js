21973f318a868622edfcb3ddfd435be1abd41b3c
rudemex <mex.skarlatina@gmail.com>
2019-03-25 15:49:26 -0300

    First Commit 2

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default class extends React.Component{
     render() {
         return (
             <div>
-                <h1>Hola Mundo!</h1>
+                <h1>¡Hola Mundo!</h1>
                 <p>Bienvenido al curso de Next.js</p>
                 <p>Este es un test.</p>
             </div>
