4e0079d8ef5376d8d4770720ffb1cc67a4c14724
Maximiliano Delgado <mdelgado@baufest.com>
2019-03-25 15:14:13 -0300

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hola Mundo!</h1>
+                <p>Bienvenido al curso de Next.js</p>
+            </div>
+        )
+    }
+}
